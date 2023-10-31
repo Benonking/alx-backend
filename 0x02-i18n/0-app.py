b10d9713e1e80bd90b4e4ed3bb5253efa8710b50
Benonking <benonkighoma@gmail.com>
2023-10-31 15:12:38 +0300

    render html template Flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env pyhton3
+'''
+A basic flask app
+'''
 from flask import Flask, render_template
 
 
@@ -11,4 +15,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
