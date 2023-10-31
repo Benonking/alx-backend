05e8dbc20091ffbb6c73c32be00acb4db2033916
Benonking <benonkighoma@gmail.com>
2023-10-31 15:04:41 +0300

    render html template Flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def dell():
-    title = "Welcome to Holberton"
+def hello():
+    '''index page'''
+    
     h1 = "Hello World"
-    return render_template('0-index.html', title=title, h1=h1)
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
