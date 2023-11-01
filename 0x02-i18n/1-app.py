a0b75716621711284b9b90bfaf26eef8674234b5
Benonking <benonkighoma@gmail.com>
2023-11-01 09:14:48 +0300

    basic babel set up

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,7 +3,7 @@
 A basic flask app
 '''
 from flask import Flask, render_template
-from  flask_babel import Babel
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -16,6 +16,7 @@ class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 babel = Babel(app)
