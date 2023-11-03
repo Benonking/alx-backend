304184316dd8b1223e7a3791932cc1a1ab0afbb8
Benonking <benonkighoma@gmail.com>
2023-11-03 12:33:05 +0300

    get locale from diff users

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+'''
+A basic flask app
+'''
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Dict, Union
+import flask
+
+app = Flask(__name__)
+
+
+class Config:
+    '''
+    config class to store avalable languages and time zone defaults
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    '''retrive a user '''
+    id = request.args.get('login_as')
+    if id:
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    '''set user before request'''
+    g.user = get_user()
+
+
+@app.route("/", strict_slashes=False)
+def hello() -> str:
+    '''index page'''
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
